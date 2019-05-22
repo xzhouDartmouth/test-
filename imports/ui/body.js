8bb6612030054c17f40a4ef2e28df02ba24fd89a
xiaozhou.zhou@dartmouth.edu <xiaozhou.zhou@dartmouth.edu>
2019-05-22 15:59:29 -0400

    add mongodb

---
+++ imports/ui/body.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+import './body.html';
+ 
+Template.body.helpers({
+ tasks() {
+    return Tasks.find({});
+  },
+});
